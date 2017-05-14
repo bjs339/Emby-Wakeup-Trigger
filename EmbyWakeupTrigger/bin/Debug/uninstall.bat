8f69e943b5b8741710a75c108238955773c1f573
Blair Shaman <bjs339@yahoo.com>
2017-05-13 20:28:05 -0400

    Remove build output

--- EmbyWakeupTrigger/bin/Debug/uninstall.bat
+++
@@ -1,2 +0,0 @@
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe /u EmbyWakeupTrigger.exe
-pause
--- EmbyWakeupTrigger/bin/Release/install.bat
+++
@@ -1,3 +0,0 @@
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe EmbyWakeupTrigger.exe
-net start "Emby Wakeup Trigger"
-pause
